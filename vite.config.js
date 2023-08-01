cad2104a19f55c0954c217604b8c6b4f077c19d4
khanshot <abdurakhman.022@gmail.com>
2023-08-01 14:50:59 +0600

    as

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/style.css',
+                'resources/js/main.js',
+                'resources/js/jquery.min.js'
             ],
             refresh: true,
         }),
